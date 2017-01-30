ae3944bf9f7fe2460018b45f366811e2c5f796d8
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-30 15:09:22 -0500

    added dynamic image tag to the route 'kitten'. Created new instance method in controller pages that defines local varable 'requested size' and defines instance variable @kitten_url which will change dynamically based on the size input

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get '/about' => 'pages#about'
 
   get'/contest' => 'pages#contest'
+
+  get '/kitten/:size' => 'pages#kitten'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
