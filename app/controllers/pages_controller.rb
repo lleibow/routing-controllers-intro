7d76d1fec9f7b6af705c6b95821bbdcba383f365
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-30 14:49:13 -0500

    entered instance variable values for @header. @header is used on the layout page. I gave it a diff value in each controller method, for each different page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "Uh oh, it's too much cheese."
   end
 
   def about
+    @header = "Grapes are the poor man's plums"
   end
 
   def contest
+    @header = "Hey, it's not a contest"
   end
 
 end
