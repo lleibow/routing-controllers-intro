39efdd89c2fa40414129187dbe116f0ec33818c9
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-30 14:35:23 -0500

    added about route to routes and made an about method in pages controller. The abut method is empty. It is taking advantage of the fact that controller methods want to render erb files that share their names

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/welcome' => 'pages#welcome'
 
   get '/' => 'pages#welcome'
+
+  get '/about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
